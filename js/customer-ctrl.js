a06c24c1546ede5d59c4778ebbeafe73a401116c
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2020-11-18 01:50:13 +0530

    Manipulate DOM using Javascript and jQuery
    add form validation and related methods.
    add btnSave related functions.
    add btnClear related functions.
    add sweetalert2 as div dependencies.
    add functions to display customer sample data in the table.

--- js/customer-ctrl.js
+++ js/customer-ctrl.js
@@ -31,7 +31,30 @@
  * Global Variables
  *===============================================================================*/
 
-// Todo: add all global variable declaration here
+// customer related variables
+var customers = [];
+var newCustomerObject;
+
+// form related elements
+var txtIdElement;
+var txtNameElement;
+var txtAddressElement;
+
+//form button
+var btnSave;
+var btnClear;
+
+// form related values are stored using these variables
+var txtIdValue;
+var txtNameValue;
+var txtAddressValue;
+
+//table related elements
+var tblCustomersElement;
+var tBodyElement;
+
+
+
 
 /*===============================================================================
  * Init
@@ -40,22 +63,215 @@
 init();
 
 function init() {
-    txtId = $('txt-id');
-    txtName = $('txt-name');
-    txtAddress = $('txt-address');
-    tblCustomers = $('tbl-customers');
+    txtIdElement = $('#txt-id');
+    txtNameElement = $('#txt-name');
+    txtAddressElement = $('#txt-address');
+
+    btnSave = $("#btn-save");
+    btnClear = $("#btn-clear");
+
+    tblCustomersElement = $('#tbl-customers');
+    tBodyElement = $('#tbl-customers>tbody');
 
     txtId.focus();
+
+    // display all the customers in the list
+    displayAllCustomers();
 }
 
 /*===============================================================================
  * Event Handlers and Timers
  *===============================================================================*/
 
-// Todo: add all event listeners and handlers here
+// Bind an event handler to btn save
+btnSave.click(insertCustomer());
+
+// Bind an event handler to btn save
+btnClear.click();
 
 /*===============================================================================
  * Functions
  *===============================================================================*/
 
-// Todo: add all functions
+/* Constructor function */
+function Customer(id, name, address) {
+    this.__id = id;
+    this.__name = name;
+    this.__address = address;
+
+    // this.printDetails = function () {
+    //     console.log(this.id, this.name, this.address);
+    // }
+}
+
+// getters and setters
+Customer.prototype.setId = function (id) {
+    this.__id = id;
+}
+
+Customer.prototype.getId = function () {
+    return this.__id;
+}
+
+Customer.prototype.setName = function (name) {
+    this.__name = name;
+}
+
+Customer.prototype.getName = function () {
+    return this.__name;
+}
+
+Customer.prototype.setAddress = function (address) {
+    this.__address = address;
+}
+
+Customer.prototype.getAddress = function () {
+    return this.__address;
+}
+
+// print customer details function
+Customer.prototype.printDetails = function () {
+    console.log('Customer details: ' + this.__id, this.__name, this.__address);
+}
+
+
+// sample data for testing purposes
+var c1 = new Customer(1, "Dhanusha", "Kelaniya");
+var c2 = new Customer(2, "Buddhika", "Colombo");
+var c3 = new Customer(3, "Sandaruwan", "Kiribathgoda");
+var c4 = new Customer(4, "Perera", "Gampaha");
+
+// make the customer list using an array
+customers.push(c1, c2, c3, c4);
+
+// print all customers for testing purposes
+for (var i = 0; i < customers.length; i++) {
+    customers[i].printDetails();
+}
+
+
+function insertCustomer() {
+    if(validate()){
+        /* validation is passed */
+
+        // create new customer object
+        newCustomerObject = createNewCustomerObject();
+
+        // add new customer object to the customers list
+        customers.push(newCustomerObject);
+
+    } else {
+        //validation failed
+        return;
+    }
+}
+
+function updateCustomer() {
+
+}
+
+function deleteCustomer(id) {
+
+}
+
+
+function getUserInputFromAllFormFields() {
+    // get user input values from the form
+    txtIdValue = txtIdElement.val().trim();
+    txtNameValue = txtNameElement.val().trim();
+    txtAddressValue = txtAddressElement.val().trim();
+}
+
+
+function createNewCustomerObject() {
+    return new Customer(txtIdValue,txtNameValue,txtAddressValue);
+}
+
+function validate() {
+    var regExp = null;
+    var validated = true;
+
+    getUserInputFromAllFormFields();
+
+    // remove error indication from the UI
+    txtIdElement.removeClass('is-invalid');
+    txtNameElement.removeClass('is-invalid');
+    txtAddressElement.removeClass('is-invalid');
+
+
+    // input fields validation process
+    if (txtAddressValue.length < 3) {
+        txtAddressElement.addClass('is-invalid');
+        txtAddressElement.select();
+        validated = false;
+    }
+
+    regExp = /^[A-Za-z][A-Za-z .]{3,}$/;
+    if (!regExp.test(txtNameValue)) {
+        txtNameElement.addClass('is-invalid');
+        txtNameElement.select();
+        validated = false;
+    }
+
+    regExp = /^C\d{3}$/;
+    if (!regExp.test(txtIdValue)) {
+        txtIdElement.addClass('is-invalid');
+        $('#helper-txt-id').removeClass('text-muted');
+        $('#helper-txt-id').addClass('invalid-feedback');
+        txtIdElement.select();
+        validated = false;
+    }
+
+    /* Let's find whether duplicate ids are there */
+    if (customers.findIndex(function (c) {
+        return c.getId() === txtIdValue;
+    }) !== -1) {
+        alert("Duplicate Customer IDs are not allowed");
+        txtIdElement.addClass('is-invalid');
+        $('#helper-txt-id').removeClass('text-muted');
+        $('#helper-txt-id').addClass('invalid-feedback');
+        txtIdElement.select();
+        validated = false;
+    }
+
+    return validated;
+}
+
+
+function clearFormFields() {
+    txtIdElement.text('');
+    txtNameElement.text('');
+    txtAddressElement.text('');
+}
+
+function displayAllCustomers() {
+
+    if (customers.length > 0) {
+
+        // remove the tfoot
+        $("#tbl-customers>tfoot").remove();
+
+        for (var i = 0; i < customers.length; i++) {
+
+            // make the table row
+            var tblRowElm = '         <tr>\n' +
+                '                        <td>' + customers[i].getId() + '</td>\n' +
+                '                        <td>' + customers[i].getName() + '</td>\n' +
+                '                        <td>' + customers[i].getAddress() + '</td>\n' +
+                '                        <td><img class="trash" src="./img/trash.png" alt="trash-icon"></td>\n' +
+                '                    </tr>';
+
+
+            // append the table row to the tbody
+            tBodyElement.append(tblRowElm);
+        }
+    } else {
+        // add the tfoot
+        $("#tbl-customers>tfoot").add();
+    }
+
+
+}
+
+
+
